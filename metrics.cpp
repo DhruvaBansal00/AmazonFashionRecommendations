030d16f32c8e561b269669660bf5cd7fb6c33e48
Dhruva Bansal <dbansal36@gatech.edu>
2019-12-26 22:15:25 +0530

    Implement mean absolute error

--- metrics.cpp
+++ metrics.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
-#include <unordered_map>
-#include <string>
-#include <vector>
+#include "metrics.h"
 
 using namespace std;
+
+double MAE(vector<Prediction> predictions) {
+    uint abs_err = 0;
+    uint num = 0;
+    for (Prediction p : predictions) {
+        abs_err += abs(stoi(p.actual_rating) - stoi(p.estimated_rating));
+        num++;
+    }
+    return abs_err/num;
+}
--- metrics.h
+++ metrics.h
@@ -1,5 +1,22 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+#include "readDataset.h"
 
 using namespace std;
+typedef struct {
+    string user_id;
+    string product_id;
+    string estimated_rating;
+    string actual_rating;
+}Prediction;
+
+double MAE(vector<Prediction>);
+double RMSE(vector<Prediction>);
+unordered_map<string,vector<double>> topN(vector<Prediction>, int n);
+double hitRate(unordered_map<string,vector<double>> topN_per_user, vector<Prediction> left_out_predictions);
+double cummlativeHitRate(unordered_map<string,vector<double>> topN_per_user, vector<Prediction> left_out_predictions, int ratingCutoff);
+void ratingHitRate(unordered_map<string,vector<double>> topN_per_user, vector<Prediction> left_out_predictions);
+double averageReciprocalHitRank(unordered_map<string,vector<double>> topN_per_user, vector<Prediction> left_out_predictions);
+double userCoverage(unordered_map<string,vector<double>> topN_per_user, int numUsers, int ratingCutoff);
+double novelty(unordered_map<string,vector<double>> topN_per_user, ReadData *dataset);

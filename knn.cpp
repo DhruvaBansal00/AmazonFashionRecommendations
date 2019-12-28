aeededb6db1ee7d8a2047d680188c95b1e02edc6
Dhruva Bansal <dbansal36@gatech.edu>
2019-12-28 20:26:46 +0530

    Computer Similarity

--- knn.cpp
+++ knn.cpp
@@ -1,5 +1,6 @@
 #include "math.h"
 #include "knn.h"
+#include <unordered_set>
 
 ContentKNN::ContentKNN(ReadData *dataset) {
     similarity_scores = new unordered_map<string, unordered_map<string, double>*>();
@@ -12,9 +13,33 @@ ContentKNN::~ContentKNN() {
     }
     delete similarity_scores;
 }
-void ContentKNN::compute_similarity(ReadData *dataset) {
 
+double category_similarity(vector<int>* product1, vector<int>* product2) {
+    int sumxx = (*product1).size();
+    int sumyy = (*product2).size();
+    unordered_set<int> product2_categories((*product2).begin(), (*product2).begin());
+    int sumxy = 0;
+    for (int cat1 : *product1) {
+        if (product2_categories.count(cat1) != 0) {
+            sumxy++;
+        }
+    }
+    return double(sumxy)/sqrt(sumxx*sumyy);
 }
+
+void ContentKNN::compute_similarity(ReadData *dataset) {
+    for (auto const & pair1 : *(*dataset).id_to_category_vector) {
+        for (auto const & pair2 : *(*dataset).id_to_category_vector) {
+            if (pair1.first.compare(pair2.first) != 0) {
+                if ((*(*similarity_scores)[pair1.first]).count(pair2.first) == 0) {
+                    (*similarity_scores)[pair1.first] = new unordered_map<string, double>();
+                }
+                (*(*similarity_scores)[pair1.first])[pair2.first] = category_similarity(pair1.first, pair2.first);
+            }
+        }
+    }
+}
+
 double ContentKNN::estimate_rating(string user, string item) {
     return 0.0;
 }
